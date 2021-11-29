0853eb4f7734505a466c62440e6b77eb1651112a
ishaannayak <87868926+ishaannayak@users.noreply.github.com>
2021-11-28 19:12:52 -0600

    Add files via upload

--- js/cave.js
+++ js/cave.js
@@ -3,16 +3,166 @@ class Cave {
         this.wall1 = null 
         this.wall2 = null 
         this.wall3 = null
+        this.wall4 = null
+        this.wall5 = null
+        this.wall6 = null
+        this.wall7 = null
+        this.wall8 = null
+        this.wall9 = null
+        this.wall10 = null
+        this.wall11 = null
+        this.wall12 = null
+        this.wall13 = null
+        this.wall14 = null
+        this.wall15 = null
+        this.wall16 = null
+        this.wall17 = null
+        this.wall18 = null
+        this.wall19 = null
+        this.wall20 = null
+        this.wall21 = null
+        this.wall22 = null
+        this.wall23 = null
+        this.wall24 = null
+        this.wall25 = null
+        this.wall26 = null
+        this.wall27 = null
+        this.wall28 = null
+        this.wall29 = null
+        this.wall30 = null
+        this.wall31 = null
+        this.wall32 = null
+        this.wall33 = null
+        this.wall34 = null
+        this.wall35 = null
+        this.wall36 = null
+        this.wall37 = null
+        this.wall38 = null
+        this.wall39 = null
+        this.wall40 = null
+
 
     }
     display(){
-        this.wall1 = createSprite(10,325,15,600)
-        this.wall1.shapeColor = "white"
+        this.wall1 = createSprite(8,260,15,450)
+        this.wall1.shapeColor = "red"
 
-        this.wall2 = createSprite(218,33,400,15)
-        this.wall2.shapeColor = "white"
+        this.wall2 = createSprite(216,33,1800,15)
+        this.wall2.shapeColor = "red"
 
-        this.wall3 = createSprite(10,800,5,130)
-        this.wall3.shapeColor = "white"
+        this.wall3 = createSprite(8,800,15,330)
+        this.wall3.shapeColor = "red"
+
+        this.wall4 = createSprite(465,912,900,15)
+        this.wall4.shapeColor = "red"
+
+        this.wall5 = createSprite(1550,912,1400,15)
+        this.wall5.shapeColor = "red"
+
+        this.wall6 = createSprite(1912,325,15,600)
+        this.wall6.shapeColor = "red"
+
+        this.wall7 = createSprite(1912,605,15,700)
+        this.wall7.shapeColor = "red"
+
+        this.wall8 = createSprite(1800,33,1100,15)
+        this.wall8.shapeColor = "red"
+
+        this.wall9 = createSprite(115,490,230,15)
+        this.wall9.shapeColor = "red"
+
+        this.wall10 = createSprite(225,500,15,280)
+        this.wall10.shapeColor = "red"
+
+        this.wall11 = createSprite(170,750,330,15)
+        this.wall11.shapeColor = "red"
+
+        this.wall12 = createSprite(335,433,15,650)
+        this.wall12.shapeColor = "red"
+
+        this.wall13 = createSprite(442,110,230,15)
+        this.wall13.shapeColor = "red"
+
+        this.wall14 = createSprite(278,270,122,15)
+        this.wall14.shapeColor = "red"
+
+        this.wall15 = createSprite(60,200,100,15)
+        this.wall15.shapeColor = "red"
+
+        this.wall16 = createSprite(115,143,15,130)
+        this.wall16.shapeColor = "red"
+
+        this.wall17 = createSprite(550,187,15,170)
+        this.wall17.shapeColor = "red"
+
+        this.wall18 = createSprite(800,110,200,15)
+        this.wall18.shapeColor = "red"
+
+        this.wall19 = createSprite(700,352,15,500)
+        this.wall19.shapeColor = "red"
+
+        this.wall20 = createSprite(550,570,15,373)
+        this.wall20.shapeColor = "red"
+
+        this.wall21 = createSprite(500,264,100,15)
+        this.wall21.shapeColor = "red"
+
+        this.wall22 = createSprite(407,750,300,15)
+        this.wall22.shapeColor = "red"
+
+        this.wall23 = createSprite(450,657,15,170)
+        this.wall23.shapeColor = "red"
+
+        this.wall24 = createSprite(792,600,200,15)
+        this.wall24.shapeColor = "red"
+
+        this.wall25 = createSprite(792,750,200,15)
+        this.wall25.shapeColor = "red"
+
+        this.wall26 = createSprite(692,824,15, 164)
+        this.wall26.shapeColor = "red"
+
+        this.wall27 = createSprite(792,600,200,15)
+        this.wall27.shapeColor = "red"
+        
+        this.wall28 = createSprite(892,508,15,200)
+        this.wall28.shapeColor = "red"
+
+        this.wall29 = createSprite(1108,150,15,250)
+        this.wall29.shapeColor = "red"
+
+        this.wall30 = createSprite(792,600,200,15)
+        this.wall30.shapeColor = "red"
+
+        this.wall31 = createSprite(800,530,15,130)
+        this.wall31.shapeColor = "red"
+        
+        this.wall32 = createSprite(900,267,400,15)
+        this.wall32.shapeColor = "red"
+
+        this.wall33 = createSprite(1002,508,15,200)
+        this.wall33.shapeColor = "red"
+
+        this.wall34 = createSprite(1194,408,400,15)
+        this.wall34.shapeColor = "red"
+
+        this.wall35 = createSprite(1002,824,15,164)
+        this.wall35.shapeColor = "red"
+
+        this.wall36 = createSprite(1150,750,300,15)
+        this.wall36.shapeColor = "red"
+
+        this.wall37 = createSprite(1150,508,15,200)
+        this.wall37.shapeColor = "red"
+        
+        this.wall38 = createSprite(1386,325,15,150)
+        this.wall38.shapeColor = "red"
+        
+        this.wall39 = createSprite(1322,242,144,15)
+        this.wall39.shapeColor = "red"
+
+        this.wall40 = createSprite(1258,142,15,203)
+        this.wall40.shapeColor = "red"
+    
    }
 }
